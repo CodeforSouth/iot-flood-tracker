bab504e2918c8f225a497ead7e69b58ddd12a10b
Bryce Kerley <bkerley@brycekerley.net>
2019-02-18 10:24:58 -0500

    bunch of working changes

--- iotfloodtracker.ino
+++ iotfloodtracker.ino
@@ -2,11 +2,14 @@
 #include <HC_SR04.h>
 
 /* defines */
-#define SAMPLE_RATE_MILLIS (15 * 1000) // how often program should sample
-#define REPORT_RATE_MILLIS (60 * 60 * 1000) // how often program should report if delta is with in REPORT_DELTA_CM
+#define SAMPLE_RATE_MILLIS (60 * 1000) // how often program should sample
+#define REPORT_RATE_SEC (15 * 60)
+#define REPORT_RATE_MILLIS (REPORT_RATE_SEC * 1000) // how often program should report if delta is with in REPORT_DELTA_CM
 #define REPORT_DELTA_CM 1.0 // if the difference between a sample taken to the previous one is greater then this value program should report the sample
 
-double last_reported_sample = -2.0;
+#define RECONNECT_SPIN_MILLIS 1000
+
+double last_reported_sample = -3.0;
 double delta = 0.0; // the difference between last sample and then one before that
 
 unsigned long lastReportTime = 0; // marker for when the last report was taken
@@ -17,6 +20,7 @@ int echoPin = D5;
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 HC_SR04 rangefinder = HC_SR04(trigPin, echoPin);
+CellularSignal signal = Cellular.RSSI();
 
 void setup()
 {
@@ -28,29 +32,45 @@ void setup()
 
 void publish(double sample, unsigned long timestamp) {
     Serial.printf("%.2fcm\tat %d\n", sample, timestamp);
-    Particle.publish("level_mm", String::format("%d", (int)(sample * 10.0)), NO_ACK);
+    Particle.publish("level_mm", String::format("%d", (int)(sample * 10.0)), PUBLIC | NO_ACK);
 }
 
 void loop()
 {
     // take a sample
     double current_sample = rangefinder.getDistanceCM();
+    
+    unsigned long now = millis();
+    
+    Serial.printf("%.2fcm\tat %d\treport in %ds\n", 
+                  current_sample,
+                  now,
+                  (REPORT_RATE_MILLIS - (now - lastReportTime)) / 1000);
+    
     // calc and set new delta
     delta = abs(last_reported_sample - current_sample);
     // get time since program started 
-    unsigned long now = millis();
+    
+    bool been_a_while = ((now - lastReportTime) >= REPORT_RATE_MILLIS);
+    bool big_change = (delta > REPORT_DELTA_CM);
     
     // publish if REPORT_RATE_MILLIS has passed or delta is bigger then REPORT_DELTA_CM
-    if( ((now - lastReportTime) >= REPORT_RATE_MILLIS) || (delta > REPORT_DELTA_CM) ) {
-        publish(last_reported_sample, now);
+    if( been_a_while || big_change ) {
+        publish(current_sample, now);
+        Serial.printf("published\n");
         lastReportTime = now;
     }
-    if (delta > REPORT_DELTA_CM) {
+    if (big_change) {
         last_reported_sample = current_sample;
     }
     
     blinkLed();
-    delay(SAMPLE_RATE_MILLIS);    
+    if (been_a_while) {
+        // picking pin d3 and RISING mode arbitrarily
+        System.sleep(SLEEP_MODE_DEEP, REPORT_RATE_SEC);
+    } else {
+        delay(SAMPLE_RATE_MILLIS);    
+    }
 }
 
 void blinkLed() {
